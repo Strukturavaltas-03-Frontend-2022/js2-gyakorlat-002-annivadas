ad965eb3fb9e736b112becc2df9728af6c41aaeb
annivadas <anni.vadas@gmail.com>
2022-08-19 20:18:20 +0200

    nem jó még

---
+++ 02/solution/js/numericConverter.js
@@ -0,0 +1,19 @@
+2/*. feladat
+- A `02/solution/js/numericConverter.js` fájlban dolgozz!
+- Készíts egy függvényt `numericConverter` néven!
+- Ez a paraméterként megadott tízes számrendszerbeli számot átkonvertálja kettes, nyolcas és tizenhatos
+számrendszerbe is.
+- Ezeket az értékeket egy objektumban add vissza. A property-k neve legyen: binary, octal, hexa. */
+const numericConverter = (decimal) => {
+    return {
+    binary: decimal.toString(2),
+    octal: decimal.toString(8),
+    hexa: decimal.toString(16)
+    };
+};
+
+
+
+export {
+    numericConverter,
+}
